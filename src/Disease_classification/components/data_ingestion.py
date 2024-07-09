a511161a4de36d3bc5f6894a67a1a0617b1e01da
ayushkum1310 <ayushpripl@gmail.com>
2024-07-10 00:25:56 +0530

    modified data transformation

--- src/Disease_classification/components/data_ingestion.py
+++ src/Disease_classification/components/data_ingestion.py
@@ -21,7 +21,7 @@ class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
     raw_data_path:str=os.path.join('artifacts','raw.csv')
-    label_preproxcessor:str=os.path.join('artifacts','label.pkl')
+    label_preproxcessor:str=os.path.join('artifacts','Disease_label.pkl')
 
 class DataIngestion:
     def __init__(self):
@@ -36,14 +36,15 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             lr=LabelEncoder()
             df['Disease']=lr.fit_transform(df['Disease'])
+            
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42,shuffle=True)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-            logging.info("Data Ingestion is completed")
             save_object(self.ingestion_config.label_preproxcessor,lr)
+            logging.info("Data Ingestion is completed")
+           
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
--- src/Disease_classification/components/data_transformation.py
+++ src/Disease_classification/components/data_transformation.py
@@ -12,15 +12,11 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from src.Disease_classification.logger import logging
 from src.Disease_classification.exception import CustomException
 from src.Disease_classification.utils import save_object
-
-
 from dataclasses import dataclass
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file = os.path.join('artifacts', 'preprocessor.pkl')
-
-
 
 class DataTransformation:
     def __init__(self):
@@ -38,9 +34,7 @@ class DataTransformation:
                 'Gender',
                 'Blood Pressure',
                 'Cholesterol Level',
-                'Outcome Variable'
             ]
-            
 
             pipe1 = Pipeline(steps=[
                 ("imputer", SimpleImputer(strategy='median')),
@@ -52,15 +46,12 @@ class DataTransformation:
                 ("encoder", OneHotEncoder(handle_unknown='ignore'))
             ])
 
-           
-
             final_preprocessor = ColumnTransformer(
                 transformers=[
                     ('num', pipe1, num),
                     ('cat', pipe2, one_hot_cols),
-                    
                 ],
-                remainder='passthrough'
+                remainder='passthrough'  # This will keep the 'Disease' column after LabelEncoder
             )
 
             return final_preprocessor
@@ -74,14 +65,41 @@ class DataTransformation:
             test_data = pd.read_csv(test_path)
             logging.info("Reading data frames")
 
+            # Initialize the preprocessor
             preprocessor = self.data_transformation_obj()
+            
+            # Apply LabelEncoder to 'Disease' and 'Outcome Variable'
+            
 
-            train_ = preprocessor.fit_transform(train_data)
-            test_ = preprocessor.transform(test_data)
+            input_train = train_data.drop(columns=['Outcome Variable'], axis=1)
+            target_train = train_data['Outcome Variable'].map({'Positive':1,'Negative':0})
+            input_test = test_data.drop(columns=['Outcome Variable'], axis=1)
+            target_test = test_data['Outcome Variable'].map({'Positive':1,'Negative':0})
 
+            # Fit and transform the training data
+            train_ = preprocessor.fit_transform(input_train)
+            # Transform the test data
+            test_ = preprocessor.transform(input_test)
+
+            # Concatenate the 'Disease' column back to the transformed data
+            train_arr = np.c_[input_train['Disease'].values.reshape(-1, 1),train_,  np.array(target_train)]
+            test_arr = np.c_[input_test['Disease'].values.reshape(-1, 1),test_,  np.array(target_test)]
+
+            pd.DataFrame(train_arr).to_csv('Hale1.csv')
             save_object(self.data_trans_config.preprocessor_obj_file, preprocessor)
 
-            return (train_, test_, self.data_trans_config.preprocessor_obj_file)
+            return train_arr, test_arr, self.data_trans_config.preprocessor_obj_file
 
         except Exception as e:
             raise CustomException(e, sys)
+
+# Example usage
+if __name__ == "__main__":
+    train_path = "train.csv"
+    test_path = "test.csv"
+    data_transformer = DataTransformation()
+    train_arr, test_arr, preprocessor_file = data_transformer.initiate_data_transformation(train_path, test_path)
+    print("Train array:")
+    print(train_arr)
+    print("\nTest array:")
+    print(test_arr)
